f5a01d273560258328e7adbb1248832916af41e9
pester33 <130249635+pester33@users.noreply.github.com>
2026-07-30 23:33:33 +0400

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,7 +1,9 @@
 const LIST = [
   "spiral2005"
 ];
+
 const LEVELS_DATA = {
+
   "spiral2005": {
     "id": 139077917,
     "name": "Spiral 2005",
@@ -13,31 +15,39 @@ const LEVELS_DATA = {
     "password": "Free To Copy",
     "records": []
   }
+
 };
+
 const STAFF = [
+
   {
     "role": "Owner",
     "name": "TuffTheo",
     "link": "https://www.youtube.com/@TuffTheo"
   },
+
   {
     "role": "Admin",
     "name": "Valtuh3636",
     "link": "https://www.youtube.com/@WALTUH3636"
   },
+
   {
     "role": "Admin",
     "name": "Nevlo",
     "link": "https://www.youtube.com/channel/UC1FU8iD-xd_zuATLXf1pH1w"
   },
+
   {
     "role": "Helper",
     "name": "Null",
     "link": ""
   },
+
   {
     "role": "Developer",
     "name": "Pester44",
     "link": "https://www.youtube.com/@pester22"
   }
+
 ];
