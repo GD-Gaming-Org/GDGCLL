67d6a3cb648d6e14c761513e754ec7fa76bad06d
pester33 <130249635+pester33@users.noreply.github.com>
2026-07-31 14:01:09 +0400

    Update data.js

--- data/data.js
+++ data/data.js
@@ -27,20 +27,20 @@ const STAFF = [
   },
 
   {
-    "role": "Owner",
+    "role": "Admin",
     "name": "Valtuh3636",
     "link": "https://www.youtube.com/@WALTUH3636"
   },
 
   {
-    "role": "Owner",
+    "role": "Admin",
     "name": "Nevlo",
     "link": "https://www.youtube.com/channel/UC1FU8iD-xd_zuATLXf1pH1w"
   },
 
   {
-    "role": ".",
-    "name": ".",
+    "role": "Helper",
+    "name": "Null",
     "link": ""
   },
 
