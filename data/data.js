ce01d3f48260f2d8635bcc1137aa33a0b7eea20b
pester33 <130249635+pester33@users.noreply.github.com>
2026-08-09 02:17:36 +0400

    Refactor staff roles and add avatars object
    Removed redundant object definitions for staff roles and added a new avatars object.

--- data/data.js
+++ data/data.js
@@ -5,91 +5,38 @@ const LIST = [
 const LEVELS_DATA = {
 
   "blackblizzard1": {
-    "id": 145957744,
-    "name": "Black Blizzard Lv1",
-    "author": "Yolkstar",
-    "creators": ["Yolkstar"],
-    "verifier": "Yolkstar",
-    "verification": "https://youtu.be/RXoyU6oMkBA",
-    "percentToQualify": 50,
-    "password": "Free To Copy",
-    "records": [
-      {
-        "user": "Alminx",
-        "percent": 100,
-        "link": "https://youtu.be/GF5vlqJNGNU",
-        "mobile": false
-      },
-      {
-        "user": "TowerOfDaBaby_Gaming",
-        "percent": 100,
-        "link": "https://youtu.be/hnXPFszV4VU",
-        "mobile": false
-      },
-      {
-        "user": "rtyrekguy",
-        "percent": 100,
-        "link": "https://youtu.be/PkHIPfulm_A",
-        "mobile": true
-      },
-      {
-        "user": "Proton53",
-        "percent": 100,
-        "link": "https://youtu.be/IIdKsRJSbXQ",
-        "mobile": false
-      },
-      {
-        "user": "neoplist",
-        "percent": 100,
-        "link": "https://youtu.be/Q6IqNdbsyRM",
-        "mobile": false
-      },
-      {
-        "user": "Canadianboi",
-        "percent": 100,
-        "link": "https://youtu.be/ZZCRVWur-o4",
-        "mobile": false
-      }
+    id: 145957744,
+    name: "Black Blizzard Lv1",
+    creators: ["Yolkstar"],
+    verifier: "Yolkstar",
+    verification: "https://youtu.be/RXoyU6oMkBA",
+    percentToQualify: 50,
+    password: "Free To Copy",
+    records: [
+      { user: "Alminx",               percent: 100, link: "https://youtu.be/GF5vlqJNGNU", mobile: false },
+      { user: "TowerOfDaBaby_Gaming", percent: 100, link: "https://youtu.be/hnXPFszV4VU", mobile: false },
+      { user: "rtyrekguy",            percent: 100, link: "https://youtu.be/PkHIPfulm_A", mobile: true  },
+      { user: "Proton53",             percent: 100, link: "https://youtu.be/IIdKsRJSbXQ", mobile: false },
+      { user: "neoplist",             percent: 100, link: "https://youtu.be/Q6IqNdbsyRM", mobile: false },
+      { user: "Canadianboi",          percent: 100, link: "https://youtu.be/ZZCRVWur-o4", mobile: false },
+      { user: "polopolo",             percent: 100, link: "https://youtu.be/WMKVw2AOixI", mobile: false }
     ]
   }
 
 };
 
 const STAFF = [
-
-  {
-    "role": "Owner",
-    "name": "TuffTheo",
-    "key": "59364368ce70ef95",
-    "link": "https://www.youtube.com/@TuffTheo"
-  },
-
-  {
-    "role": "Owner",
-    "name": "Valtuh3636",
-    "key": "e8f7e6dc86e8b5f9",
-    "link": "https://www.youtube.com/@WALTUH3636"
-  },
-
-  {
-    "role": "Owner",
-    "name": "Nevlo",
-    "key": "6fccfcaa6e51e4e2",
-    "link": "https://www.youtube.com/channel/UC1FU8iD-xd_zuATLXf1pH1w"
-  },
-
-  {
-    "role": "Helper",
-    "name": "Null",
-    "key": "",
-    "link": ""
-  },
-
-  {
-    "role": "Developer",
-    "name": "Pester44",
-    "key": "e2e7b77571e19f26",
-    "link": "https://www.youtube.com/@pester22"
-  }
-
+  { role: "Owner",     name: "TuffTheo",   key: "59364368ce70ef95", link: "https://www.youtube.com/@TuffTheo" },
+  { role: "Owner",     name: "Valtuh3636", key: "e8f7e6dc86e8b5f9", link: "https://www.youtube.com/@WALTUH3636" },
+  { role: "Owner",     name: "Nevlo",      key: "6fccfcaa6e51e4e2", link: "https://www.youtube.com/channel/UC1FU8iD-xd_zuATLXf1pH1w" },
+  { role: "Admin",     name: "rtyrekguy",  key: "", link: "" },
+  { role: "Admin",     name: "Nothing",    key: "", link: "" },
+  { role: "Helper",    name: "Null",       key: "", link: "" },
+  { role: "Developer", name: "Pester44",   key: "e2e7b77571e19f26", link: "https://www.youtube.com/@pester22" }
 ];
+
+const AVATARS = {
+  "Nevlo": "icons/nevlo.png",
+  "Pester44": "icons/pester44.png"
+};
+
