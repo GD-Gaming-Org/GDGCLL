df1d661c91c1ea0c887b88adc794402ce07af254
pester33 <130249635+pester33@users.noreply.github.com>
2026-08-14 23:41:43 +0400

    Add user registration and login functions

--- js/login.js
+++ js/login.js
@@ -1,1 +1,18 @@
+async function registerUser(username, password) {
+  const res = await fetch("/register.php", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+  return res.json();
+}
 
+async function loginUser(username, password) {
+  const res = await fetch("/login.php", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify({ username, password })
+  });
+  return res.json();
+}
